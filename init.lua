3bb7e10570f2225737f37b93782fb8d8a48bdeec
shivajiva101 <shivajiva101@hotmail.com>
2018-04-18 22:24:45 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,27 +1,58 @@
 --[[
 original code provided by tenplus1
 This mod instigates an effective border for new players joining a server
-with persistence of the last state across server restarts
+with persistence of the last state and visa records across server restarts
 ]]
 
 local mod_data = minetest.get_mod_storage()
-local border = "OPEN"
-local visa = {}
+local border = mod_data:get_string("status")
+local visa = mod_data:get_string("visa")
+local duration = minetest.setting_get("border.visa_duration") or 86400 -- 1 day
+local msg = minetest.setting_get("border.msg") or "\nSorry, no new players being admitted at this time!"
 
--- initialise
-if mod_data:get_string("status") == "" then
-  mod_data:set_string("status", "CLOSED") 
+if border == "" then
+	-- initialise
+	mod_data:set_string("status", "CLOSED")
+	border = "CLOSED"
 end
 
---set
-border = mod_data:get_string("status")
+local function update_visa_cache(name)
+	--if name exists in cache remove it
+	if visa[name] then
+		visa[name] = nil
+		collectgarbage()
+	else
+		-- cache visa
+		visa[name] = os.time() + duration 
+	end
+	mod_data:set_string("visa", minetest.serialize(visa))
+end
+
+if visa == "" then
+	-- initialise
+	visa = {}
+else
+	-- load visa table
+	visa = minetest.deserialize(visa)
+	-- iterate
+	for name,expires in pairs(visa) do
+		local t_remains = expires - os.time()
+		if t_remains > 0 then
+			--set
+			minetest.after(t_remains, update_visa_cache, name)
+		else
+			-- remove
+			update_visa_cache(name)
+		end
+	end
+end
 
 -- announce status
 minetest.after(5, function()
 	minetest.chat_send_all("[border:info] border is "..border)
 end)
 
--- toggle new players
+-- chat command to toggle the border status
 minetest.register_chatcommand("border", {
     params = "",
     description = "Toggles if new players are allowed",
@@ -34,10 +65,11 @@ minetest.register_chatcommand("border", {
         border = "CLOSED"
         minetest.chat_send_player(name, "[border:info] refusing new players.")
       end
-      mod_data:set_string("flag", border) -- save
+      mod_data:set_string("status", border) -- save current state
     end
   })
 
+-- add visa
 minetest.register_chatcommand("visa", {
 	params = "player",
 	description = "Adds a temporary visa allowing a new player to create an account",
@@ -46,26 +78,23 @@ minetest.register_chatcommand("visa", {
 		if not param then
 			minetest.chat_send_player(name, "Use: /visa <name>")
 		end
-		if not visa[param] then
-			visa[param] = true
-			minetest.after(300, function(name)
-				if visa[name] then visa[name] = nil end
-				end, param)
-		end
+		update_visa_cache(param)
+		minetest.after(duration, update_visa_cache, param)
+		minetest.chat_send_player(name, "A visa was issued to "..param)
     end
   })
 
--- register hook
+-- register callback
 minetest.register_on_prejoinplayer(function(name, ip)
 	-- owner exception
 	if minetest.setting_get("name") == name then
 			return
 	end
-	-- stop NEW players from joining
-	local player = minetest.get_auth_handler().get_auth(name)
-	if border == "CLOSED" and not player and not visa[name] then
-			return ("\nSorry, no new players being admitted at this time!")
+	-- stop NEW players from joining unless they have a visa
+	local player_exists = minetest.get_auth_handler().get_auth(name)
+	if border == "CLOSED" and not player_exists and not visa[name] then
+			return msg
 	end
-	if visa[name] then visa[name] = nil end
+	if visa[name] then update_visa_cache(name) end -- remove visa
 end
 )
